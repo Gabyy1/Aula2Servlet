77f1f522257d3eb21868559836fda828f951dbec
Gabrizinha <gabiemanu18@gmail.com>
2023-03-03 21:54:37 -0300

    commit

---
+++ src/main/java/persistence/IViagemDao.java
@@ -0,0 +1,12 @@
+package persistence;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import model.Onibus;
+import model.Viagem;
+
+public interface IViagemDao {
+	public Viagem consultarViagem(Viagem v) throws SQLException, ClassNotFoundException;
+}
+
